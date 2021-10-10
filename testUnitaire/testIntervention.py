c4b3e142a9264122b1f258e99176ffa976f38a54
ThibautRingeval <78933047+ThibautRingeval@users.noreply.github.com>
2021-10-10 10:02:44 +0200

    Add files via upload
    Création des test unitaire (environ 50% terminé)

---
+++ testUnitaire/testIntervention.py
@@ -0,0 +1,10 @@
+import unittest
+from ..manipulationDB.intervention import Intervention
+
+class InterventionTestCase(unittest.TestCase):
+    def Creer_Intervention(self):
+        inter1 = Intervention(1 , 1 , "a" , "a", True, "a")
+        self.assertIsNotNone(inter1)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ testUnitaire/testRequeteDB.py
@@ -0,0 +1,14 @@
+import unittest
+from ..manipulationDB.requetesDB import RequetesDB
+
+class RequeteDBTestCase(unittest.TestCase):
+    def Creer_requete(self):
+        requete1 = RequetesDB("interventions.db")
+        self.
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
