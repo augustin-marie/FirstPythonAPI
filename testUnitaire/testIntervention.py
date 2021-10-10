73f6baf8baf4a95cf611bdbab66f63c9e0e1cc23
PRENOM NOM <EMAIL@DOMAINE>
2021-10-10 21:03:37 +0200

    [integration des fichier init]

--- testUnitaire/testIntervention.py
+++ testUnitaire/testIntervention.py
@@ -6,5 +6,7 @@ class InterventionTestCase(unittest.TestCase):
         inter1 = Intervention(1 , 1 , "a" , "a", True, "a")
         self.assertIsNotNone(inter1)
 
+
+
 if __name__ == '__main__':
     unittest.main()
