1cc22587203bc0554b6b3cdf1b8421c6a66b50d4
Augustin Marie <a.marie189@laposte.net>
2021-10-11 13:23:11 +0200

    Modifications pour tenter de faire marcher les imports

--- API/fonctionsAPI.py
+++ API/fonctionsAPI.py
@@ -7,13 +7,13 @@ app = Flask(__name__)
 
 @app.route('/api/afficher')
 def afficher():
-    sql = RequetesDB("./interventions.db")
-    sqlLecture = f"SELECT interventionTest.* FROM interventionTest"
+    sql = RequetesDB("../interventions.db")
+    sqlLecture = f"SELECT * FROM interventions"
     sql.executerCommande(sqlLecture)
 
     dicoInterventions={}
     for row in sql.cursor:
-        dicoInterventions[row[0]] = Intervention(row[1], row[2], row[3], row[4], row[5], row[6])
+        dicoInterventions[row[0]] = str(Intervention(row[1], row[2], row[3], row[4], row[5], row[6]))
 
     return jsonify(dicoInterventions)
 
